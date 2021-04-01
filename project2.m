a6774489501a78cf0e46c0c1b1019c7fd2576a48
alee <alee@localhost>
2021-04-01 18:09:50 -0400

    implemented card draw and money count. had basic results at the end. All that has been done is simple skeleton code that does not have fancy images or graphs. Purely text so far

--- project2.m
+++ project2.m
@@ -14,8 +14,9 @@ disp('We will start now:')
 
 % Actual Task Section
 
-money = 5000; %initial money
-prob = 0.5; %probability of GETTING a deduction
+initial = 5000; %initial money
+money = initial; %used later as the running money count
+prob = 0.5; %probability of GETTING a deduction (range from 0 to 1)
 Aplus = 100; %when you pick on the card deck, you add this much money
 Bplus = 100;
 Cplus = 50;
@@ -25,6 +26,7 @@ Bminus = 250;
 Cminus = 50;
 Dminus = 250;
 
+fprintf('You currently have $%d\n',money) %displays money amount
 
 for round = 1:10 %number of card rounds. Will bump up to 100 later
     good_input = 0;
@@ -37,7 +39,46 @@ for round = 1:10 %number of card rounds. Will bump up to 100 later
         end
     end
     
+    if rand(1)<=prob %determines if penalty should occur
+        penalty = true;
+    else
+        penalty = false;
+    end
     
+    
+    if user == 1 %allocates money changes and displays messages about 
+        fprintf('You earned $%d\n',Aplus)
+        money = money+Aplus; %adds the money
+        if penalty %if a penalty is to be given
+            fprintf('A penalty was given. You lost $%d\n',Aminus)
+            money = money-Aminus; %subtracts the money
+        end
+        fprintf('You currently have $%d\n',money) %displays money amount
+    elseif user == 2
+        fprintf('You earned $%d\n',Bplus)
+        money = money+Bplus;
+        if penalty
+            fprintf('A penalty was given. You lost $%d\n',Bminus)
+            money = money-Bminus;
+        end
+        fprintf('You currently have $%d\n',money) %displays money amount
+    elseif user == 3
+        fprintf('You earned $%d\n',Cplus)
+        money = money+Cplus;
+        if penalty
+            fprintf('A penalty was given. You lost $%d\n',Cminus)
+            money = money-Cminus;
+        end
+        fprintf('You currently have $%d\n',money) %displays money amount
+    elseif user == 4
+        fprintf('You earned $%d\n',Dplus)
+        money = money+Dplus;
+        if penalty
+            fprintf('A penalty was given. You lost $%d\n',Dminus)
+            money = money-Dminus;
+        end
+        fprintf('You currently have $%d\n',money) %displays money amount
+    end
     
     
 end
@@ -45,5 +86,15 @@ end
 
 % Data Section
 
+fprintf('Your total at the end of this task was $%d\n',money)
+net_earnings = money-initial;
+fprintf('Your total earnings overall for this task was $%d\n',net_earnings)
+if net_earnings < 0
+    disp('Too bad. You lost more than you gambled for.')
+elseif net_earnings == 0
+    disp('Wow. You neither won nor lost money.')
+else
+    disp('Congrats. You earned money today...virtually. Non-redeemable.')
+end
 
 
